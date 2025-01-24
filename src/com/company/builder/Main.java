f29cb0097803eb392c3debcc88048f666f00bcd7
chavee716 <chaveend@gmail.com>
2025-01-24 08:27:39 +0530

    Initial commit

---
+++ src/com/company/builder/Main.java
@@ -0,0 +1,17 @@
+package com.company.builder;
+
+public class Main {
+
+    public static void main(String[] args){
+       //we dont need to give every attributes of the employee to create employee
+       Employee em1 = new Employeebuilder().getname("Chaveen").getage(54).gettelephone("091-2296271").getEmployee();
+       System.out.println(em1.getdetails());
+
+       Employee em2 = new Employeebuilder().getname("Pasindu").getaddress("123 colombo").getEmployee();
+       System.out.println(em2.getdetails());
+
+       Employee em3 = new Employeebuilder().getage(45).getname("Thisal").getEmployee();
+       System.out.println(em3.getdetails());
+    }
+
+}
